96bb555df60189a9e0cef05b0a3d2b67736e5004
practicas1234567 <168371488+practicas1234567@users.noreply.github.com>
2024-05-01 21:40:38 +0200

    Add files via upload

--- js/script_old.js
+++ js/script_old.js
@@ -1,3 +1,4 @@
+//assistant
 let timer;
 document.addEventListener("DOMContentLoaded", function() {
     timer = setTimeout(showAssistant, 5000);
@@ -7,28 +8,32 @@ function showAssistant() {
     clearTimeout(timer);
     document.getElementById("assistant").style.display = "block";
     
-     //Agrega un evento de clic para ocultar el asistente al hacer clic en él
+     //Add a click event to hide the assistant when clicking on it
      document.getElementById("assistant").addEventListener("click", hideAssistantOnClick);
     }
     
-function hideAssistantOnClick() {
-    document.getElementById("assistant").style.display = "none";
-}
+    function hideAssistantOnClick() {
+        document.getElementById("assistant").style.display = "none";
+    }
+
+
 
 // External Carousell
+
 
 function adjustImageSize(value) {
     var images = document.querySelectorAll('.carousel-item img');
     value = parseInt(value);
     images.forEach(function(image) {
-        image.style.maxWidth = value + '%'; // Ajusta el ancho máximo de la imagen
-        image.style.height = 'auto'; // Mantiene la proporción
+        image.style.maxWidth = value + '%'; // Adjust witdh image max
+        image.style.height = 'auto'; // Mantain  proportinal rate
     });
 }
 
-// Fetch data from nas20.json
 
-function updateFooter(data) {
+  // Fetch data from nas20.json
+  
+  function updateFooter(data) {
     const nasdaqScroll = document.getElementById('nasdaq-scroll');
     nasdaqScroll.innerHTML = '';
 
@@ -39,9 +44,9 @@ function updateFooter(data) {
       // Set color based on change value
       let textColorClass = '';
       if (change > 0) {
-        textColorClass = 'text-success'; // Verde para cambio positivo
+        textColorClass = 'text-success'; // Green to positive change
       } else if (change < 0) {
-        textColorClass = 'text-danger'; // Rojo para cambio negativo
+        textColorClass = 'text-danger'; // Red to negative change
       }
 
       // Populate row with company information
@@ -54,129 +59,128 @@ function updateFooter(data) {
       `;
       nasdaqScroll.innerHTML += companyInfo;
     });
-}
-
-// Fetch data from nas20.json
-fetch('/json/nas20.json')
-  .then(response => response.json())
-  .then(data => {
-    const totalCompanies = data.length;
-    let index = 0;
-
-    // Update footer with first 2 companies
-    updateFooter(data.slice(index, index + 2));
-
-    // Update footer every 2 seconds
-    setInterval(() => {
-      index = (index + 2) % totalCompanies;
-      updateFooter(data.slice(index, index + 2));
-    }, 2000);
-  })
-  .catch(error => console.error('Error fetching data:', error));
-
-
-// URL JSON enterprises
-var jsonUrl = "/json/nas20.json";
-
-// Function to read JSON and fill table enterprises
-function fetchAndPopulateEnterprises() {
-    fetch(jsonUrl)
-        .then(response => response.json())
-        .then(data => {
-            populateEnterprises(data);
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
-}
-
-// Function to refill table enterprises
-function populateEnterprises(enterprises) {
-    var enterpriseList = document.getElementById("enterprise-list");
-    var row;
-    for (var i = 0; i < enterprises.length; i++) {
-        var enterprise = enterprises[i];
-        if (i % 2 === 0) {
-            // Create a new row every two enterprises
-            row = document.createElement("tr");
-        }
-        var companyCell = document.createElement("td");
-        companyCell.textContent = enterprise.company; // enterprise name without link
-        var websiteCell = document.createElement("td");
-        var link = document.createElement("a");
-        link.href = enterprise.url;
-        link.textContent = enterprise.url.replace(/(^\w+:|^)\/\//, ''); // Show URL without http:// or https://
-        link.target = "_blank"; // Open link in a new window
-        websiteCell.appendChild(link);
-        row.appendChild(companyCell);
-        row.appendChild(websiteCell);
-        if (i % 2 === 1 || i === enterprises.length - 1) {
-            // Append row to the table when two enterprises are pushed or the last one.
-            enterpriseList.appendChild(row);
-        }
-    }
-}
-
-// Call function to fetch JSON and refill enterprises table.
-fetchAndPopulateEnterprises();
-
-// Graphics scripts
-$(document).ready(function() {
-  var jsonData;
-  $.getJSON("/json/nas20.json", function(data) {
-    jsonData = data;
-    drawChart("min");
-  });
-
-  $("#btnMin").click(function() {
-    drawChart("min");
-  });
-
-  $("#btnMax").click(function() {
-    drawChart("max");
-  });
-
-  $("#btnChange").click(function() {
-    drawChart("change");
-  });
-
-  // Draw graph from JSON data
-  function drawChart(dataType) {
-    var chartData = [];
-    for (var i = 0; i < jsonData.length; i++) {
-      var item = jsonData[i];
-      var color;
-      if (dataType === "min") {
-        color = (item.min === item[dataType]) ? "red" : "green";
-      } else if (dataType === "max") {
-        color = (item.max === item[dataType]) ? "green" : "red";
-      } else if (dataType === "change") {
-        color = (parseFloat(item.change) >= 0) ? "green" : "red";
-      }
-      var value = (dataType === "change") ? parseFloat(item.change) : item[dataType];
-      chartData.push({ label: item.symbol, y: value, color: color });
-    }
-    var chart = new CanvasJS.Chart("chartContainer", {
-      animationEnabled: true,
-      theme: "light2",
-      title: {
-        text: "Values " + dataType
-      },
-      axisX: {
-        title: "Company Symbol"
-      },
-      axisY: {
-        title: (dataType === "change") ? "Change (%)" : "Value (USD)"
-      },
-      data: [{
-        type: "column",
-        dataPoints: chartData
-      }]
-    });
-
-    chart.render();
   }
-})
 
-// URL JSON images gallery
+  // Fetch data from nas20.json
+  fetch('/json/nas20.json')
+    .then(response => response.json())
+    .then(data => {
+      const totalCompanies = data.length;
+      let index = 0;
+
+      // Update footer with first 2 companies
+      updateFooter(data.slice(index, index + 2));
+
+      // Update footer every 2 seconds
+      setInterval(() => {
+        index = (index + 2) % totalCompanies;
+        updateFooter(data.slice(index, index + 2));
+      }, 2000);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+
+        // URL  JSON  enterprises
+        var jsonUrl = "/json/nas20.json";
+
+        // Function to read  JSON and fill table enterprises
+        function fetchAndPopulateEnterprises() {
+            fetch(jsonUrl)
+                .then(response => response.json())
+                .then(data => {
+                    populateEnterprises(data);
+                })
+                .catch(error => console.error('Error fetching JSON:', error));
+        }
+
+        // Function to refill table enterprises
+        function populateEnterprises(enterprises) {
+            var enterpriseList = document.getElementById("enterprise-list");
+            var row;
+            for (var i = 0; i < enterprises.length; i++) {
+                var enterprise = enterprises[i];
+                if (i % 2 === 0) {
+                    // every two enterprises, create a new  row
+                    row = document.createElement("tr");
+                }
+                var companyCell = document.createElement("td");
+                companyCell.textContent = enterprise.company; // enterprise name without link
+                var websiteCell = document.createElement("td");
+                var link = document.createElement("a");
+                link.href = enterprise.url;
+                link.textContent = enterprise.url.replace(/(^\w+:|^)\/\//, ''); // Show l'URL whithout http:// o https://
+                link.target = "_blank"; // new link in a new window
+                websiteCell.appendChild(link);
+                row.appendChild(companyCell);
+                row.appendChild(websiteCell);
+                if (i % 2 === 1 || i === enterprises.length - 1) {
+                    // fill row at the table end when push 2 enterprises or the last one.
+                    enterpriseList.appendChild(row);
+                }
+            }
+        }
+         // call function JSON and refill enterprises table.
+        fetchAndPopulateEnterprises();
+
+        // graphics scripts          
+        $(document).ready(function() {
+          var jsonData;
+          $.getJSON("/json/nas20.json", function(data) {
+            jsonData = data;
+            drawChart("min");
+          });
+    
+          $("#btnMin").click(function() {
+            drawChart("min");
+          });
+    
+          $("#btnMax").click(function() {
+            drawChart("max");
+          });
+    
+          $("#btnChange").click(function() {
+            drawChart("change");
+          });
+    
+        // Draw graph from json data
+        function drawChart(dataType) {
+          var chartData = [];
+          for (var i = 0; i < jsonData.length; i++) {
+            var item = jsonData[i];
+            var color;
+            if (dataType === "min") {
+              color = (item.min === item[dataType]) ? "red" : "green";
+            } else if (dataType === "max") {
+              color = (item.max === item[dataType]) ? "green" : "red";
+            } else if (dataType === "change") {
+              color = (parseFloat(item.change) >= 0) ? "green" : "red";
+            }
+            var value = (dataType === "change") ? parseFloat(item.change) : item[dataType];
+            chartData.push({ label: item.symbol, y: value, color: color });
+          }
+          var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            theme: "light2",
+            title: {
+              text: "Values " + dataType
+            },
+            axisX: {
+              title: "Company Symbol"
+            },
+            axisY: {
+              title: (dataType === "change") ? "Change (%)" : "Value (USD)"
+            },
+            data: [{
+              type: "column",
+              dataPoints: chartData
+            }]
+          });
+        
+          chart.render();
+        }
+        })
+        
+ // URL JSON images gallery
 
 var jsonPath = "/json/nas20.json";
 
@@ -205,7 +209,7 @@ function populateGallery(images, imagesPerRow) {
             imagesInCurrentRow = 0;
         }
         var imageElement = document.createElement("div");
-        imageElement.classList.add("col-md-" + (12 / imagesPerRow)); // Modify rows based on the number of images
+        imageElement.classList.add("col-md-" + (12 / imagesPerRow)); // Modify rows in function of number of images
         imageElement.classList.add("image-item-" + index);
         var img = document.createElement("img");
         img.src = image.path;
@@ -264,20 +268,20 @@ $(document).ready(function() {
     });
 });
 
-// Load images from JSON
+//  Javascrip to load images from JSON
 
-function fetchAndPopulateCarousel() {
+  function fetchAndPopulateCarousel() {
     fetch("/json/nas20.json")
       .then(response => response.json())
       .then(data => {
         populateCarousel(data);
       })
       .catch(error => console.error('Error fetching JSON:', error));
-}
+  }
 
-function populateCarousel(images) {
+  function populateCarousel(images) {
     var carouselInner = document.querySelector(".carousel-inner");
-    carouselInner.innerHTML = ''; // Clear content from carousel
+    carouselInner.innerHTML = ''; // Clear content from  carousel
     images.forEach((image, index) => {
       var carouselItem = document.createElement("div");
       carouselItem.classList.add("carousel-item");
@@ -292,13 +296,37 @@ function populateCarousel(images) {
       carouselItem.appendChild(img);
       carouselInner.appendChild(carouselItem);
     });
-}
+  }
 
-document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     fetchAndPopulateCarousel();
-});
+  });
 
-// Clients function
+ //clients function
+  
+ /*document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  // Retrieve last comments from JSON
+  fetch('/json/clients.json')
+    .then(response => response.json())
+    .then(data => {
+      if (matchingClients.length > 0) {
+      const matchingClients = data.filter(client => client.name === name && client.mail === email); // Clients filter thatmatch with nomber and email from formulary
+        const lastClient = matchingClients[matchingClients.length - 1]; // Get the last client that mach
+        const lastComment = lastClient.message; // Get  the last commnet from client
+        const interest = lastClient.invest; //Get cliet interest
+        const interestText = getInterestText(interest);
+        const combinedText = `Last Comment:\n${lastComment}\n\nInterest:\n${interestText}`;
+        document.getElementById('lastComments').value = combinedText;
+      } else {
+        document.getElementById('lastComments').value = 'No comments found for the provided name and email.';
+      }
+    })
+    .catch(error => console.error('Error retrieving last comments:', error)); // Print any errors to the console
+});*/
+
 document.getElementById('contactForm').addEventListener('submit', function(event) {
   event.preventDefault();
   const name = document.getElementById('name').value;
@@ -307,7 +335,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
   fetch('/json/clients.json')
     .then(response => response.json())
     .then(data => {
-      const matchingClients = data.filter(client => client.name === name && client.mail === email); // Clients filter that match with name and email from the form
+      const matchingClients = data.filter(client => client.name === name && client.mail === email); // Mover esta línea aquí
       if (matchingClients.length > 0) {
         const lastClient = matchingClients[matchingClients.length - 1]; // Get the last client that matches
         const lastComment = lastClient.message; // Get the last comment from client
@@ -332,11 +360,10 @@ function getInterestText(interest) {
     case 1:
       return '(1,000-10,000$) - Initial investment.';
     case 2:
-      return '(10,000-100,000$) - Mid-range investments.';
+      return '(10,000-100,000$) -  Mid-range investments.';
     case 3:
-      return '1 million or more - Large-scale investments.';
+      return '1 million or more - large-scale investments.';
     default:
       return '';
   }
 }
-//code validated by https://validator.w3.org/#validate_by_input 
